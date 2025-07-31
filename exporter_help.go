af17ed542d6e593f4b4172b134252803bf2dec67
Sergei <mihailov.s@outlook.com>
2025-07-31 22:08:41 +0200

    chore: flag descriptions updates

--- exporter_help.go
+++ exporter_help.go
@@ -8,12 +8,12 @@ Commands:
 discover-ports:
   Show discovered ports and exit
 loop-textfile:
-  Writes all metrics to textfiles every loop-interval
+  Writes all metrics to textfile every loop-interval
     --loop-textfile-update-interval=30s
-        Interval between textfiles updates
+        Interval between textfile updates
 
 single-textfile:
-  Writes all metrics to textfiles ONCE. Usefull for testing or crons
+  Writes all metrics to textfile ONCE. Usefull for testing or crons
 
 
 Flags:
@@ -26,7 +26,7 @@ Ethtool settings:
 Various paths settings:
   --linux-net-class-path=/sys/class/net
   --textfile-directory=/var/lib/node-exporter/textfiles
-    Path to node_exporter textfile directory. Only used in 'single-textfile' and 'loop-textfile' modes
+    Path to the node_exporter textfile directory. Only used in 'single-textfile' and 'loop-textfile' modes
 
 Collectors, enabled by default:
   --no-collect-generic-info-settings
@@ -44,16 +44,16 @@ Port detection settings:
   --discover-allowed-port-types=1,
     Comma-separated list of allowed interface types (see if_arp.h). Set to empty ('') to allow all port types
   --discover-all-ports
-    Force discover all ports, ignoring all the other discover flags, EXCEPT for 'discover-allowed-port-types' and 'discover-ports-regexp'
+    Force discover all the ports, ignoring all the other discover flags, EXCEPT for 'discover-allowed-port-types' and 'discover-ports-regexp'
   --no-discover-bond-slaves
-    Whether we discover ports that are enslaved by bonds
+    Whether to discover ports that are enslaved by bonds
   --discover-bridge-slaves
-    Whether we discover ports that are enslaved by bridges
+    Whether to discover ports that are enslaved by bridges
   --discover-ports-regexp=.+
     Only discover ports with names matching this regexp
 
 Absent metrics settings:
   --keep-absent-metrics
-    Set 'Nan' value for every metric that wasn't found
+    Set 'Nan' value for every metric that was not found
 
 `
